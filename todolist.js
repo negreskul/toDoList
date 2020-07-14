e7cf0bd01a72eca764d98b38ed95ebbd00674e43
Alexandra Negreskul <negreskula@gmail.com>
2020-07-14 22:16:41 +0300

    Initial commit

---
+++ todolist.js
@@ -0,0 +1,39 @@
+$(function() {
+
+	function toDo(title, description) {
+		if($('#emptylist-text').css('display') === 'block'){
+			$('#emptylist-text').css('display', 'none');
+		}
+		var container = $('<div class="todo-container"><p class="todo-title">' + title + 
+			'</p><button class="todo-delete">X</button><div class="todo-description">' 
+			+ description + '</div></div>');
+
+		container.insertAfter('#todolisttitle');
+
+		$('.todo-delete', container).on('click', function() {
+			$(this).parent().remove();
+			i--;
+			console.log(i);
+			if(i < 1) {
+				$('#emptylist-text').css('display', 'block');
+			}
+		});
+	}
+
+	var i = 0;
+
+	$('#addbutton').on('click', function() {
+		var title = $('#addtitle').val();
+		var description = $('#addtext').val();
+		if(title != 0 && description != 0) {
+			toDo(title, description);
+			$('#addtitle').val('');
+			$('#addtext').val('');
+			i++;
+			console.log(i);
+		}
+	});
+
+	
+
+});
